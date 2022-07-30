9838aee2cccd4f62d217ad64e504975fb05511a0
buckl <969154615@qq.com>
2022-07-30 12:52:44 +0800

    add

--- spring-cloud-eureka-server/src/test/java/com/lft/springcloudeurekaserver/SpringCloudEurekaServerApplicationTests.java
+++ spring-cloud-eureka-server/src/test/java/com/lft/springcloudeurekaserver/SpringCloudEurekaServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.lft.springcloudeurekaserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
