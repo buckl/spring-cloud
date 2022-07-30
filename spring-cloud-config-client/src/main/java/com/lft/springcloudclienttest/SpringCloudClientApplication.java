d8a744b6c73ee9f6e2928d7bf6a3773eabb8a450
buckl <969154615@qq.com>
2022-07-30 10:55:25 +0800

    add

---
+++ spring-cloud-config-client/src/main/java/com/lft/springcloudclienttest/SpringCloudClientApplication.java
@@ -0,0 +1,46 @@
+package com.lft.springcloudclienttest;
+
+import com.lft.springcloudclienttest.health.MyHealthIndicator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Set;
+
+@SpringBootApplication
+public class SpringCloudClientApplication {
+
+    private final ContextRefresher contextRefresher;
+
+    private final Environment environment;
+
+    @Autowired
+    public SpringCloudClientApplication(ContextRefresher contextRefresher, Environment environment) {
+        this.contextRefresher = contextRefresher;
+        this.environment = environment;
+    }
+
+    @Scheduled(fixedRate = 5 * 1000, initialDelay = 2 * 1000)
+    public void autoRefresh() {
+        Set<String> refreshValues = contextRefresher.refresh();
+        refreshValues.forEach(data -> {
+            System.err.printf("[线程:%s ], 更新配置项目：key:%s value:%s \n", Thread.currentThread().getName(), data, environment.getProperty(data));
+        });
+
+        System.out.printf("[线程:%s ], 更新配置项目：%s \n", Thread.currentThread().getName(), refreshValues);
+    }
+
+    @Bean
+    public MyHealthIndicator myHealthIndicator() {
+        return new MyHealthIndicator();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudClientApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-config-client/src/test/java/com/lft/springcloudclienttest/SpringCloudClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lft.springcloudclienttest;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudClientTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
