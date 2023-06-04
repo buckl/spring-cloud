cf9853670166e0012c2457896baec7a64d5f0a85
buckl <969154615@qq.com>
2023-06-04 15:36:12 +0800

    aop

--- interceptor/src/main/java/com/lft/test/intercepter/aop/GlobalAopAdvice.java
+++ interceptor/src/main/java/com/lft/test/intercepter/aop/GlobalAopAdvice.java
@@ -1,8 +1,11 @@
-package com.lft.test.intercepter.controller;
+package com.lft.test.intercepter.aop;
 
+import com.alibaba.fastjson.JSONObject;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,18 +13,12 @@ import org.springframework.stereotype.Component;
 public class GlobalAopAdvice {
 
     @Pointcut("execution (* com.lft.test.intercepter.controller..*.*(..))")
-    public void test() {
+    public void controllerPointcut() {}
 
-    }
-
-    @Before("test()")
+    @Before("controllerPointcut()")
     public void beforeAdvice(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
-
-    }
-
-    @After("test()")
-    public void afterAdvice(JoinPoint joinPoint) {
-
+        String paramString = JSONObject.toJSONString(args[0]);
+        System.out.println(paramString);
     }
 }
