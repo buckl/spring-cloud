277acef3ab559aaffa1ca88ca362963dc1323c5a
buckl <969154615@qq.com>
2024-11-09 02:35:21 +0800

    2

--- interceptor/src/main/java/com/lft/test/intercepter/aop/GlobalAopAdvice.java
+++ interceptor/src/main/java/com/lft/test/intercepter/aop/GlobalAopAdvice.java
@@ -2,10 +2,7 @@ package com.lft.test.intercepter.aop;
 
 import com.alibaba.fastjson.JSONObject;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -13,7 +10,8 @@ import org.springframework.stereotype.Component;
 public class GlobalAopAdvice {
 
     @Pointcut("execution (* com.lft.test.intercepter.controller..*.*(..))")
-    public void controllerPointcut() {}
+    public void controllerPointcut() {
+    }
 
     @Before("controllerPointcut()")
     public void beforeAdvice(JoinPoint joinPoint) {
