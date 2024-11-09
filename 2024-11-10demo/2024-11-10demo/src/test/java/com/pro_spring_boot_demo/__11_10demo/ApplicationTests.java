bdddb9c0c9c01077036010cc360997a466196a95
buckl <969154615@qq.com>
2024-11-10 01:11:17 +0800

    更改

---
+++ 2024-11-10demo/2024-11-10demo/src/test/java/com/pro_spring_boot_demo/__11_10demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro_spring_boot_demo.__11_10demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
